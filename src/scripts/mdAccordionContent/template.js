5b6de89bd3606d0d6c5f5374ab19d50d166558e0
nicaf <florin.g.nica@gmail.com>
2017-04-06 11:33:50 +0300

    Fix md-svg-icon interpolation in template.js for md-accordion-content

--- src/scripts/mdAccordionContent/template.js
+++ src/scripts/mdAccordionContent/template.js
@@ -4,7 +4,7 @@ export default function() {
     <div class="md-accordion-content" layout="column">
       <md-button class="md-accordion-toggle" ng-if="$mdAccordionContent.heading" ng-click="$mdAccordionContent.changeState();" ng-class="{ 'md-active': $mdAccordionContent.visible }">
         <div layout="row">
-          <md-icon ng-if="$mdAccordionContent.svgIcon" md-svg-icon="$mdAccordionContent.svgIcon"></md-icon>
+          <md-icon ng-if="$mdAccordionContent.svgIcon" md-svg-icon="{{$mdAccordionContent.svgIcon}}"></md-icon>
           <md-icon ng-if="$mdAccordionContent.icon">{{ $mdAccordionContent.icon }}</md-icon>
           <span flex>{{ $mdAccordionContent.heading }}</span>
           <i ng-if="$mdAccordionContent.arrow" class="fa fa-chevron-down accordion-arrow" aria-hidden="true"></i>
