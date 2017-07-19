6f79521b6d1f13e5a77e7f5101afcfedac13d308
ulion <ulion2002@gmail.com>
2017-07-19 16:29:29 +0800

    Adjust to make the active class work.

--- src/scripts/mdAccordionButton/index.js
+++ src/scripts/mdAccordionButton/index.js
@@ -9,7 +9,7 @@ let directive = () => {
       ngClick: '@?',
       uiSref: '@?',
       uiSrefActive: '@?',
-      active: '@?',
+      active: '<?mdActive',
       href: '@?',
       target: '@?'
     },
